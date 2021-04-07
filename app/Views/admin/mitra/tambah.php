1bb16a997c1bf74de46018b02fb320411255901b
Badar Wildanie <badar.serpent@gmail.com>
2021-04-07 21:51:15 +0700

    Refine UI/UX tambah data Mitra admin
    Memperbaiki tampilan tambah data mitra dan menyiapkannya untuk dilakukan integrasi sistem. Yang di perbarui:
    - bug TailSelect JS: hanya bisa menghapus item satu kali sejak halaman di load (solusi dilakukan dengan menyediakan event untuk button di fungsi cbComplete / callback setelah tailselect di load )
    - menghapus field judul pada artikel profil mitra
    - menambahkan fitur auto fill on changed pada layout preview input data yang disediakan
    - menetapkan <form> untuk keseluruhan dokumen web

--- app/Views/admin/mitra/tambah.php
+++ app/Views/admin/mitra/tambah.php
@@ -2,15 +2,15 @@
 
 <?php $this->section('content'); ?>
 <div class="container-fluid mt-4">
-	<div class="row">
-		<div class="col-md-6">
-			<div class="card">
-				<div class="card-header p-3" style="line-height: 8px">
-					<h3 class="card-title mb-0 pb-0">Tambah data Mitra LPNU Malang</h3>
-					<span class="text-muted text-uppercase font-weight-bold" style="font-size: 8pt;">Masukkan identitas, gambar, dan artikel profil di formulir berikut</span>
-				</div>
-				<div class="card-body">
-					<form method="post">
+	<form action="" method="">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="card">
+					<div class="card-header p-3" style="line-height: 8px">
+						<h3 class="card-title mb-0 pb-0">Tambah data Mitra LPNU Malang</h3>
+						<span class="text-muted text-uppercase font-weight-bold" style="font-size: 8pt;">Masukkan identitas, gambar, dan artikel profil di formulir berikut</span>
+					</div>
+					<div class="card-body">
 						<div class="row">							
 							<div class="col form-group mb-1 lpnu-form">
 								<label class="form-control-label">Nama Pemilik</label>
@@ -94,7 +94,7 @@
 									<div class="input-group-prepend">
 										<span class="input-group-text"><i class="fas fa-code-branch"></i></span>
 									</div>
-									<input type="text" name="mwc_nu" class="form-control">
+									<input type="text" name="mwcnu" class="form-control">
 								</div>
 							</div>
 						</div>
@@ -112,7 +112,7 @@
 							</div>
 							<div class="col-sm-6 pl-sm-1 form-group mb-1 lpnu-form">
 								<label class="form-control-label">Kategori Usaha <a href="javascript:void(0)" class="btn-tambah-kategori" style='font-size:8pt'>[Tambah Kategori]</a></label>
-								<select id="tags-jenis_usaha" class="form-control" name="status_usaha" multiple>
+								<select id="tags-jenis_usaha" class="form-control" name="jenis_usaha" multiple>
 									<option value="Makanan & Minuman">Makanan & Minuman</option>
 									<option value="Warung Kopi">Warung Kopi</option>
 									<option value="Elektronik">Elektronik</option>
@@ -121,7 +121,7 @@
 							</div>
 						</div>
 						<div class="row">
-							<div class="form-group col-sm-6 pr-sm-1 mb-1 lpnu-form">
+							<div class="form-group col mb-1 lpnu-form">
 								<label class="form-control-label">Nama Barang</label>
 								<div class="input-group input-group-merge">
 									<div class="input-group-prepend">
@@ -129,18 +129,7 @@
 									</div>
 									<input type="text" name="nama_barang" class="form-control">
 								</div>
-							</div>
-							<div class="form-group col-sm-6 pl-sm-1 mb-1 lpnu-form">
-								<label class="form-control-label">Ada Izin?</label>
-								<div class="input-group input-group-merge">
-									<div class="input-group-prepend">
-										<span class="input-group-text"><i class="fas fa-boxes"></i></span>
-									</div>
-									<select class="form-control" name="izin">
-										<option value="Ada">Ada</option>
-										<option value="Belum ada">Belum ada</option>
-									</select>
-								</div>
+								<span class="text-muted text-xs">Anda juga bisa menambahkan lebih dari 1, <strong>Gunakan tanda , (koma) </strong> untuk memisahkan</span>
 							</div>
 						</div>
 						<div class="row">
@@ -154,140 +143,195 @@
 								</div>
 							</div>
 						</div>
-					</form>
-				</div>
-			</div> <!-- end card -->
-		</div> <!-- end col -->
-		<div class="col-md-6">
-			<div class="row">
-				<div class="col">
-					<div class="card">
-						<div class="card-header p-3" style="line-height: 8px">
-							<h4 class="card-title mb-0 pb-0">Galeri foto Mitra</h4>
-							<span class="text-muted text-uppercase" style="font-size: 8pt">Anda dapat memambahkan lebih dari 1 FSoto</span>
+						<div class="row">
+							<div class="form-group col mb-1 lpnu-form">
+								<label class="form-control-label">Ada Izin?</label>
+								<div class="input-group input-group-merge">
+									<div class="input-group-prepend">
+										<span class="input-group-text"><i class="fas fa-boxes"></i></span>
+									</div>
+									<select class="form-control" name="izin">
+										<option value="Ada">Ada</option>
+										<option value="Belum ada">Belum ada</option>
+									</select>
+								</div>
+							</div>
 						</div>
-						<div class="card-body">
-							<div class="row">
-								<div class="col">
-									<div id="dz-mitra" class="dz-wrapper">
-										<div class="dz-default dz-message">
-											<h4>SERET FILE DISINI</h4>
-							                <p>ATAU KLIK UNTUK UPLOAD</p>
+					</div>
+				</div> <!-- end card -->
+			</div> <!-- end col -->
+			<div class="col-md-6">
+				<div class="row">
+					<div class="col">
+						<div class="card">
+							<div class="card-header p-3" style="line-height: 8px">
+								<h4 class="card-title mb-0 pb-0">Galeri foto Mitra</h4>
+								<span class="text-muted text-uppercase" style="font-size: 8pt">Anda dapat memambahkan lebih dari 1 FSoto</span>
+							</div>
+							<div class="card-body">
+								<div class="row">
+									<div class="col">
+										<div id="dz-mitra" class="dz-wrapper">
+											<div class="dz-default dz-message">
+												<h4>SERET FILE DISINI</h4>
+								                <p>ATAU KLIK UNTUK UPLOAD</p>
+											</div>
 										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-					</div> <!-- end card -->
+						</div> <!-- end card -->
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="col">
-					<div class="card bg-primary text-white">
-						<div class="card-body">
-							<div class="row">
-								<div class="col-lg-6">
-									<img src="<?=site_url('images/mitra/mitra-01.jpg')?>" class="img-thumbnail">
-								</div>
-								<div class="col-lg-6">
-									<h3 class="text-white mt-2 mt-lg-0">Sholehudin Arif</h3>
-									<table class="table table-condensed text-white">
-										<tr>
-											<th>Nomor HP</th>
-											<td style="width: 15px">:</td>
-											<td><span class="fill-nomor_hp">082228111059</span></td>
-										</tr>
-										<tr>
-											<th>Kecamatan</th>
-											<td style="width: 15px">:</td>
-											<td><span class="fill-kecamatan">Gondanglegi</span></td>
-										</tr>
-										<tr>
-											<th>Kelurahan</th>
-											<td style="width: 15px">:</td>
-											<td><span class="fill-kelurahan">Putat Lor</span></td>
-										</tr>
-										<tr>
-											<th>Alamat</th>
-											<td style="width: 15px">:</td>
-											<td><span class="fill-alamat">Jl. Melati 03/01, Dsn. Krajan, Putat Lor, Gondanglegi, Malang</span></td>
-										</tr>
-										<tr>
-											<th>Ranting</th>
-											<td style="width: 15px">:</td>
-											<td><span class="fill-kelurahan">Gondanglegi</span></td>
-										</tr>
-										<tr>
-											<th>MWCNU</th>
-											<td style="width: 15px">:</td>
-											<td><span class="fill-kelurahan">Gondanglegi</span></td>
-										</tr>
-									</table>
-								</div>
-							</div> <!-- end row identity -->
-							<div class="row mt-2">
-								<div class="col">
-									<table class="table table-condensed text-white">
-										<tr>
-											<th style="max-width: 40%">Merek Dagang</th>
-											<td style="width: 15px">:</td>
-											<td><span class="fill-merek_dagang">Toserba Digital</span></td>
-										</tr>
-										<tr>
-											<th style="max-width: 40%">Peran Ekonomi</th>
-											<td style="width: 15px">:</td>
-											<td><span class="fill-status_usaha">Produsen, Agen, Retail</span></td>
-										</tr>
-										<tr>
-											<th style="max-width: 40%">Kategori Usaha</th>
-											<td style="width: 15px">:</td>
-											<td><span class="fill-jenis_usaha">Makanan & Minuman, Elektronika, Pecah-belah</span></td>
-										</tr>
-										<tr>
-											<th style="max-width: 40%">Nama Barang</th>
-											<td style="width: 15px">:</td>
-											<td><span class="fill-status_usaha">Keripik Munirsia, MP3 Desa Abadi</span></td>
-										</tr>
-									</table>
+				<div class="row">
+					<div class="col">
+						<div class="card bg-primary text-white">
+							<div class="card-body">
+								<div class="row">
+									<div class="col-lg-6">
+										<img src="<?=site_url('img_unavailable.png')?>" class="img-thumbnail">
+									</div>
+									<div class="col-lg-6">
+										<h3 class="text-white mt-2 mt-lg-0"><span class="fill-nama_pemilik">Nama Pemilik</span></h3>
+										<table class="table table-condensed text-white">
+											<tr>
+												<th>Nomor HP</th>
+												<td style="width: 15px">:</td>
+												<td><span class="fill-nomor_hp">-</span></td>
+											</tr>
+											<tr>
+												<th>Kecamatan</th>
+												<td style="width: 15px">:</td>
+												<td><span class="fill-kecamatan">-</span></td>
+											</tr>
+											<tr>
+												<th>Kelurahan</th>
+												<td style="width: 15px">:</td>
+												<td><span class="fill-kelurahan">-</span></td>
+											</tr>
+											<tr>
+												<th>Alamat</th>
+												<td style="width: 15px">:</td>
+												<td><span class="fill-alamat_usaha">-</span></td>
+											</tr>
+											<tr>
+												<th>Ranting</th>
+												<td style="width: 15px">:</td>
+												<td><span class="fill-ranting_nu">-</span></td>
+											</tr>
+											<tr>
+												<th>MWCNU</th>
+												<td style="width: 15px">:</td>
+												<td><span class="fill-mwcnu">-</span></td>
+											</tr>
+										</table>
+									</div>
+								</div> <!-- end row identity -->
+								<div class="row mt-2">
+									<div class="col">
+										<table class="table table-condensed text-white">
+											<tr>
+												<th style="max-width: 40%">Merek Dagang</th>
+												<td style="width: 15px">:</td>
+												<td><span class="fill-merek_dagang">-</span></td>
+											</tr>
+											<tr>
+												<th style="max-width: 40%">Peran Ekonomi</th>
+												<td style="width: 15px">:</td>
+												<td><span class="fill-status_usaha">-</span></td>
+											</tr>
+											<tr>
+												<th style="max-width: 40%">Kategori Usaha</th>
+												<td style="width: 15px">:</td>
+												<td><span class="fill-jenis_usaha">-</span></td>
+											</tr>
+											<tr>
+												<th style="max-width: 40%">Nama Barang</th>
+												<td style="width: 15px">:</td>
+												<td><span class="fill-nama_barang">-</span></td>
+											</tr>
+										</table>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</div> <!-- end col -->
-	</div> <!-- end row -->
-	<div class="row">
-		<div class="col">
-			<div class="card">
-				<div class="card-body">
-					<form>
+			</div> <!-- end col -->
+		</div> <!-- end row -->
+		<div class="row">
+			<div class="col">
+				<div class="card">
+					<div class="card-header p-3" style="line-height: 8px">
+						<h3 class="card-title mb-0 pb-0">Tulis artikel profil Mitra (opsional)</h3>
+						<span class="text-muted text-uppercase font-weight-bold" style="font-size: 8pt;">Anda dapat menuliskan sesuatu tentang profil yang anda input.</span>
+					</div>
+					<div class="card-body">
 						<div class="form-group lpnu-form">
-							<label>Judul Artikel</label>
-							<input type="text" name="judul" class="form-control" placeholder="Tuliskan judul artikel disini...">
-						</div>
-						<div class="form-group lpnu-form">
-							<label>Isi Artikel</label>
 							<input type="hidden" name="status">
 							<input type="hidden" name="list_gambar">
 							<textarea name="artikel" id="artikel-mitra" rows="8"></textarea>
 						</div>
 						<div class="form-group lpnu-form row justify-content-center">
-							<input type="submit" name="publikasikan" class="btn btn-primary col-6" value="Publikasikan">
+							<input type="submit" name="publikasikan" class="btn btn-primary col-8" value="Publikasikan">
 							<input type="button" name="simpan" class="btn btn-default ml-2 col-auto" value="Simpan sebagai Draft">
 						</div>
-					</form>
+					</div>
 				</div>
 			</div>
-		</div>
-	</div>
+		</div> <!-- end row-->
+	</form>
 </div>
 <?php $this->endSection(); ?>
 
 <?php $this->section('jsContent') ?>
 <script type="text/javascript">
 	jQuery(document).ready(function($) {
-		
+		$("[name='nama_pemilik']").change(function(e) {
+			value = $(this).val()
+			$(".fill-nama_pemilik").html(value)
+		});
+		$("[name='nomor_hp']").change(function(e) {
+			value = $(this).val()
+			$(".fill-nomor_hp").html(value)
+		});
+		$("[name='kecamatan']").change(function(e) {
+			value = $(this).val()
+			$(".fill-kecamatan").html(value)
+		});
+		$("[name='kelurahan']").change(function(e) {
+			value = $(this).val()
+			$(".fill-kelurahan").html(value)
+		});
+		$("[name='alamat_usaha']").change(function(e) {
+			value = $(this).val()
+			$(".fill-alamat_usaha").html(value)
+		});
+		$("input[name='ranting_nu']").change(function(e) {
+			value = $(this).val()
+			$(".fill-ranting_nu").html(value)
+		})
+		$("[name='mwcnu']").change(function(e) {
+			value = $(this).val()
+			$(".fill-mwcnu").html(value)
+		})
+		$("[name='merek_dagang']").change(function(e) {
+			value = $(this).val()
+			$(".fill-merek_dagang").html(value)
+		})
+		$("[name='status_usaha']").change(function(e) {
+			value = $(this).val()
+			$(".fill-status_usaha").html(value.join(', '))
+		})
+		$("[name='jenis_usaha']").change(function(e) {
+			value = $(this).val()
+			console.log(value)
+			$(".fill-jenis_usaha").html(value.join(', '))
+		})
+		$("[name='nama_barang']").change(function(e) {
+			value = $(this).val()
+			$(".fill-nama_barang").html(value)
+		})
 		var statusUsaha = tail.select('#tags-status_usaha', {
 			animate: true,
 			hideSelected: true,
@@ -303,11 +347,17 @@
 			cbLoopItem: (item, group, search) => {
 				console.log(search);
 				var newItem = document.createElement('li');
-				newItem.innerHTML = "<a href='#' class='delete-item' data-toggle='tooltip' title='Hapus kategori' data-value='" + item.value + "'><i class='fas fa-times text-danger'></i></a> " + item.value;
+				newItem.innerHTML = "<a href='#' class='delete-item btn btn-sm btn-danger rounded-circle' data-toggle='tooltip' title='Hapus kategori' data-value='" + item.value + "'><i class='fas fa-times'></i></a> " + item.value;
 				return newItem;
+			},
+			cbComplete: () => {
+				$('.delete-item').click(function(e) {
+					e.preventDefault();
+					$('#tags-jenis_usaha option[value="' +$(this).data('value')+'"]').remove()
+					kategoriUsaha.reload();
+				});
 			}
 		})
-
 		$(".btn-tambah-kategori").popover({
 	        html: true,
 	        trigger: 'click',
