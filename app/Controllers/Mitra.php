dae857f14893721e6ffe61eae45f3576f5cfb422
Badar Wildanie <badar.serpent@gmail.com>
2021-04-08 23:54:12 +0700

    Front end detail Mitra sudah dinamis dan menampilkan konten dari sistem
    Sangat simple, hanya menambahkan nilai bawaan ke masing-masing elemen data yang disediakan oleh UI Designer. Seperti sebelumnya, MitraModel digunakan. Namun ada beberapa hal yang masih kurang, yaitu menampilkan data penulis dan widget

--- app/Controllers/Mitra.php
+++ app/Controllers/Mitra.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
+use App\models\MitraModel;
 
 class Mitra extends Controller
 {
@@ -16,25 +17,32 @@ class Mitra extends Controller
 		
 	}
 
-	public function detail($slug)
+	public function detail($id = false)
 	{
-		$data['ui_title'] = "Toserba Mulyadi Elektro - Mitra LPNU Malang";
-		$data['ui_css'] = [
-			"lib/light-slider/css/lightslider.min.css"
-		];
-		$data['ui_js'] = [
-			"lib/light-slider/js/lightslider.min.js"
-		];
-		$data['ui_navbar'] = [
-			"Home|fas fa-home|". base_url(),
-			"Mitra|fas fa-list|". site_url('mitra'),
-			"LPNU Kecamatan" => [
-				"Sub #1|fab fa-facebook|https://www.facebook.com/",
-				"Sub #2|fab fa-twitter|https://www.twitter.com/",
-				"Sub #3|fab fa-instagram|https://www.instagram.com/"
-			],
-			"Statistik|fas fa-chart-line|". site_url('mitra'),
-		];
-		return view('mitra/detail', $data);
+		$mitraModel = new MitraModel();
+		$data['mitra'] = $mitraModel->find($id);
+		if ($data['mitra'] != '') {
+			$data['ui_title'] = "Toserba Mulyadi Elektro - Mitra LPNU Malang";
+			$data['ui_css'] = [
+				"lib/light-slider/css/lightslider.min.css"
+			];
+			$data['ui_js'] = [
+				"lib/light-slider/js/lightslider.min.js"
+			];
+			$data['ui_navbar'] = [
+				"Home|fas fa-home|". base_url(),
+				"Mitra|fas fa-list|". site_url('mitra'),
+				"LPNU Kecamatan" => [
+					"Sub #1|fab fa-facebook|https://www.facebook.com/",
+					"Sub #2|fab fa-twitter|https://www.twitter.com/",
+					"Sub #3|fab fa-instagram|https://www.instagram.com/"
+				],
+				"Statistik|fas fa-chart-line|". site_url('mitra'),
+			];
+			return view('mitra/detail', $data);
+		}
+		else {
+			return view('errors/html/error_404');
+		}
 	}
 }
--- app/Views/mitra/detail.php
+++ app/Views/mitra/detail.php
@@ -6,102 +6,106 @@
 	<div class="row">
 		<div class="col-md-6 pr-0 mr-0">
 			<ul id="mitra-gallery" class="light-slider">
-				<li data-thumb="<?=site_url('images/demo/6769867-amazing-dusk-wallpaper.jpg')?>">
-					<img src="<?=site_url('images/demo/6769867-amazing-dusk-wallpaper.jpg')?>">
+				<?php 
+					$galeri = explode('|', $mitra['galeri']);
+					foreach ($galeri as $gambar) {
+				?>
+				<li data-thumb="<?=$gambar?>">
+					<img src="<?=$gambar?>">
 				</li>
-				<li data-thumb="<?=site_url('images/demo/61466591_2504207752947117_4148325829635473408_n.jpg')?>">
-					<img src="<?=site_url('images/demo/61466591_2504207752947117_4148325829635473408_n.jpg')?>">
-				</li>
-				<li data-thumb="<?=site_url('images/demo/Annotation 2020-04-02 2208172.png')?>">
-					<img src="<?=site_url('images/demo/Annotation 2020-04-02 2208172.png')?>">
-				</li>
-				<li data-thumb="<?=site_url('images/demo/cat.jpg')?>">
-					<img src="<?=site_url('images/demo/cat.jpg')?>">
-				</li>
-
-				<li data-thumb="<?=site_url('images/demo/46988924_2227206850647210_1101064348647817216_o.jpg')?>">
-					<img src="<?=site_url('images/demo/46988924_2227206850647210_1101064348647817216_o.jpg')?>">
-				</li>
-				<li data-thumb="<?=site_url('images/demo/IMG_20210105_182056.jpg')?>">
-					<img src="<?=site_url('images/demo/IMG_20210105_182056.jpg')?>">
-				</li>
-				<li data-thumb="<?=site_url('images/demo/Screenshot 2020-12-27 091001.png')?>">
-					<img src="<?=site_url('images/demo/Screenshot 2020-12-27 091001.png')?>">
-				</li>
-				<li data-thumb="<?=site_url('images/demo/Storesys.jpg')?>">
-					<img src="<?=site_url('images/demo/Storesys.jpg')?>">
-				</li>
-				<li data-thumb="<?=site_url('images/demo/Anotasi 2019-11-24 105230.jpg')?>">
-					<img src="<?=site_url('images/demo/Anotasi 2019-11-24 105230.jpg')?>">
-				</li>
-				<li data-thumb="<?=site_url('images/demo/Anotasi 2019-12-20 133837.jpg')?>">
-					<img src="<?=site_url('images/demo/Anotasi 2019-12-20 133837.jpg')?>">
-				</li>
-				<li data-thumb="<?=site_url('images/demo/Storesys.jpg')?>">
-					<img src="<?=site_url('images/demo/Storesys.jpg')?>">
-				</li>
+				<?php 
+					}
+				?>
 			</ul>
 		</div>
 		<div class="col-md-6" style="background-color: #099543;">
 			<div class="p-3 pb-4 mt-3 mb-2 bg-dark" style="font-family: 'Raleway', sans-serif; line-height: 5px ">
-				<h2 class="text-white m-0 bg-dark text-uppercase" style="font-weight: 900">Toserba Elektronika</h2>
-				<span class="text-white pb-2">Agen, Distributor, Produsen</span>
+				<?php 
+					$judul = '';
+					if ($mitra['merek_dagang'] != '') {
+						$judul = $mitra['merek_dagang'];
+					}
+					else if ($mitra['nama_barang'] != '') {
+						$judul = $mitra['nama_barang'];
+					}
+					else if ($mitra['jenis_usaha'] != '') {
+						$jenis_usaha = explode('|', $mitra['jenis_usaha']);
+						$i = 1;
+						$judul = '';
+						foreach ($jenis_usaha as $jenis) {
+							$judul .= $jenis . ', ';
+							if ($i >= 3) {
+								break;
+							}
+							$i++;
+						}
+						rtrim($judul, ', ');
+					}
+				?>
+				<h2 class="text-white m-0 bg-dark text-uppercase" style="font-weight: 900"><?=$judul?></h2>
+				<span class="text-white pb-2"><?=join(', ', explode('|', $mitra['status_usaha']))?></span>
 			</div>
 			<table class="table text-white table-sm table-border-0">
 				<tr>
 					<th class="bg-dark d-flex flex-row align-items-center text-warning"><i class="fas fa-store pl-1 pr-2"></i>Kategori Usaha</th>
 					<td>:</td>
 					<th>
-						<span class="label label-warning font-weight-bold text-dark">Makanan & Minuman</span>
-						<span class="label label-warning font-weight-bold text-dark">Barang Elektronika</span> 
-						<span class="label label-warning font-weight-bold text-dark">Pecah Belah</span>
-						<span class="label label-warning font-weight-bold text-dark">Snack</span>
-						<span class="label label-warning font-weight-bold text-dark">Penyewaan Terop & Salon</span>
+						<?php 
+							$jenis_usaha = explode('|', $mitra['jenis_usaha']);
+							foreach ($jenis_usaha as $jenis) {
+						?>
+						<span class="label label-warning font-weight-bold text-dark"><?=$jenis?></span>
+						<?php 
+							}
+						?>
 					</td>
 				</tr>
 				<tr>
 					<th class="d-flex flex-row align-items-center"><i class="fas fa-box-open pl-1 pr-2"></i>Nama Barang</th>
 					<td>:</td>
 					<th>
-						<span class="label label-info font-weight-bold text-dark">Kripik NU</span>
-						<span class="label label-info font-weight-bold text-dark">AQNU</span> 
-						<span class="label label-info font-weight-bold text-dark">Salon Aktif</span>
+						<?php 
+							$nama_barang = explode(',', $mitra['nama_barang']);
+							foreach ($nama_barang as $barang) {
+						?>
+						<span class="label label-info font-weight-bold text-dark"><?=ltrim($barang)?></span>
+						<?php } ?>
 					</td>
 				</tr>
 				<tr>
 					<th class="bg-dark d-flex flex-row align-items-center text-info"><i class="fas fa-city pl-1 pr-2"></i>Kecamatan</th>
 					<td>:</td>
-					<th>Gondanglegi</td>
+					<th><?=$mitra['kecamatan']?></td>
 				</tr>
 				<tr>
 					<th class="d-flex flex-row align-items-center"><i class="fas fa-city pl-1 pr-2"></i>Kelurahan</th>
 					<td>:</td>
-					<th>Putat Lor</td>
+					<th><?=$mitra['kelurahan']?></td>
 				</tr>
 				<tr>
 					<th class="bg-dark d-flex flex-row align-items-center  text-success"><i class="fas fa-map-marked-alt pl-1 pr-2"></i>Alamat</th>
 					<td>:</td>
-					<th>Jl. Raya Sugi Pesantren rt.03 rw.01, Dsn. Krajan, Putat Lor, Gondanglegi, Malang.</td>
+					<th><?=$mitra['alamat_usaha']?></td>
 				</tr>
 				<tr>
 					<th class="bg-dark d-flex flex-row align-items-center text-danger"><i class="fas fa-signature pl-1 pr-2"></i>Nama Pemilik</th>
 					<td>:</td>
-					<th>Ali Sugianto.</td>
+					<th><?=$mitra['nama_pemilik']?></td>
 				</tr>
 				<tr>
 					<th class="d-flex flex-row align-items-center"><i class="fas fa-phone-alt pl-1 pr-2"></i>Nomor HP</th>
 					<td>:</td>
-					<th>082228111059</td>
+					<th><?=$mitra['nomor_hp']?></td>
 				</tr>
 				<tr>
 					<th class="bg-dark d-flex flex-row align-items-center text-primary"><i class="fas fa-code-branch pl-1 pr-2"></i>Ranting NU</th>
 					<td>:</td>
-					<th>Gondanglegi</td>
+					<th><?=$mitra['ranting_nu']?></td>
 				</tr>
 				<tr>
 					<th class="d-flex flex-row align-items-center"><i class="fas fa-sitemap pl-1 pr-2"></i>MWCNU</th>
 					<td>:</td>
-					<th>Gondanglegi</td>
+					<th><?=$mitra['mwcnu']?></td>
 				</tr>
 			</table>
 		</div> <!-- end col-6 -->
@@ -113,23 +117,22 @@
 			<div class="col-lg-8">
 				<div class="row mb-1">
 					<div class="col">
-						<h1 class="m-0" style="font-family: 'Raleway', sans-serif; font-weight: 700; font-size: 24pt">Profil Mitra LPNU Toserba Elektronika</h1>
+						<h1 class="m-0" style="font-family: 'Raleway', sans-serif; font-weight: 700; font-size: 24pt">Profil Mitra LPNU: <?=$judul?></h1>
 					</div>
 				</div>
 				<div class="row mb-2">
 					<div class="col d-flex flex-row align-items-center" style="font-size: 8pt; color: #099543; font-weight: 700;">
 						<div class="d-flex flex-row align-items-center mr-4" style="">
 							<img src="<?=site_url('images/profile/admin-default.png')?>" class="rounded-circle mr-1" style='height: 20px;'>
-							<span>Badar Wildanie</span>
+							<span><?=$mitra['admin_username']?></span>
 						</div>
-						<span class="mr-4"><i class="fas fa-calendar-alt pr-2"></i>19 Januari 2021, 20:21</span>
+						<span class="mr-4"><i class="fas fa-calendar-alt pr-2"></i><?=date('d-m-Y H:i:s', strtotime($mitra['created_at']))?></span>
 						<span class="label label-warning font-weight-bold text-dark mr-1">Profil Mitra</span>
 					</div>
 				</div>
 				<div class="row">
 					<div class="col article">
-						
-	<figure class="image image_resized image-style-align-left" style="width:52.38%;"><img src="<?=site_url('images/mitra/mitra-01.jpg')?>"></figure><p style="text-align:justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie dapibus sapien in venenatis. Duis eu venenatis justo. Quisque interdum dui ante, in porttitor tortor tincidunt a. Curabitur sagittis pulvinar sem, vitae dapibus leo dignissim ac. Donec gravida pretium vehicula. Ut vel purus quam. Sed varius scelerisque sem a porta. Phasellus sagittis vestibulum semper. Donec vulputate molestie urna quis varius. Sed placerat dui posuere arcu auctor, ac maximus lacus semper. Vestibulum cursus libero a nisl luctus, eget posuere lorem ullamcorper. Etiam elementum leo vel pulvinar tempus. Fusce luctus dictum odio, eu accumsan diam feugiat quis.</p><p style="text-align:justify;">Fusce maximus lacus blandit urna maximus, sed maximus nisl fermentum. Phasellus finibus quam nisl, eu efficitur metus vehicula sed. Maecenas tincidunt, ante in pretium porta, dui felis molestie mauris, sed tincidunt enim nisi ut eros. Quisque semper erat pretium eros efficitur facilisis. Praesent massa justo, auctor quis ex id, lobortis lobortis sem. Vivamus interdum auctor elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</p><p style="text-align:justify;"><strong>Proin vestibulum massa a odio bibendum interdum</strong>. Etiam ut <i>augue vitae felis iaculis consectetur at et nunc</i>. <i><strong>Maecenas eleifend eros a</strong></i> arcu bibendum faucibus. Aliquam finibus sollicitudin pharetra. Interdum et malesuada fames ac ante ipsum primis in faucibus. In purus mi, sagittis sed massa vel, consequat hendrerit erat. Mauris eu leo in mauris finibus ornare tempor vitae ligula. Phasellus faucibus at metus non sagittis. Donec vel fringilla lacus. Aliquam placerat orci at tempus gravida. Fusce sodales auctor erat, nec pharetra ante venenatis ut. Maecenas quis mauris non tellus blandit congue.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie dapibus sapien in venenatis. Duis eu venenatis justo. Quisque interdum dui ante, in porttitor tortor tincidunt a. Curabitur sagittis pulvinar sem, vitae dapibus leo dignissim ac. Donec gravida pretium vehicula. Ut vel purus quam. Sed varius scelerisque sem a porta. Phasellus sagittis vestibulum semper. Donec vulputate molestie urna quis varius. Sed placerat dui posuere arcu auctor, ac maximus lacus semper. Vestibulum cursus libero a nisl luctus, eget posuere lorem ullamcorper. Etiam elementum leo vel pulvinar tempus. Fusce luctus dictum odio, eu accumsan diam feugiat quis.</p><h4 style="text-align:justify;"><strong>Daftar anggota</strong></h4><figure class="table"><table><tbody><tr><td><strong>Nama Anggota</strong></td><td><strong>Alamat</strong></td><td><strong>Nomor HP</strong></td></tr><tr><td>Supri Yanto</td><td>Jl. Melati 03/01, Dsn. Krajan, Putat Lor, Gondanglegi, Malang.</td><td>0822228111059</td></tr><tr><td>Supri Kaduts</td><td>Jl. Raya Sudirman, Kasin, Malang.</td><td>0831229481297</td></tr></tbody></table></figure><p style="text-align:justify;">Fusce maximus lacus blandit urna maximus, sed maximus nisl fermentum. Phasellus finibus quam nisl, eu efficitur metus vehicula sed. Maecenas tincidunt, ante in pretium porta, dui felis molestie mauris, sed tincidunt enim nisi ut eros. Quisque semper erat pretium eros efficitur facilisis. Praesent massa justo, auctor quis ex id, lobortis lobortis sem. Vivamus interdum auctor elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</p><ol><li style="text-align:justify;">auctor quis ex id, l</li><li style="text-align:justify;">eu efficitur metus vehicula sed. Maecenas tincidunt,&nbsp;</li><li style="text-align:justify;">asds</li></ol><p style="text-align:justify;">dum. Etiam ut augue vitae felis iaculis consectetur at et nunc. Maecenas eleifend eros a arcu bibendum faucibus. Aliquam finibus sollicitudin pharetra. Interdum et malesuada fames ac ante ipsum primis in faucibus. In purus mi, sagittis sed massa vel, consequat hendrerit erat. Mauris eu leo in mauris&nbsp;</p><ul><li style="text-align:justify;">Vestibulum</li><li style="text-align:justify;">Aido</li><li style="text-align:justify;">Acutor Ecit</li></ul><p style="text-align:justify;">us sollicitudin pharetra. Interdum et malesuada fames ac ante ipsum primis in faucibus. In purus mi, sagittis sed massa vel, consequat hendrerit erat. Mauris eu lus sollicitudin pharetra. Interdum et malesuada fames ac ante ipsum primis in faucibus. In purus mi, sagittis sed massa vel, consequat hendrerit erat. Mauris eu l</p><blockquote><p style="text-align:justify;">io bibendum interdum. Etiam ut augue vitae felis iaculis consectetur at eio bibendum interdum. Etiam ut augue vitae felis iaculis consectetur at eio bibendum interdum. Etiam ut augue vitae felis iaculis consectetur at e.</p></blockquote><p style="text-align:justify;">Proin vestibulum massa a odio bibendum interdum. Etiam ut augue vitae felis iaculis consectetur at et nunc. Maecenas eleifend eros a arcu bibendum faucibus. Aliquam finibus sollicitudin pharetra. Interdum et malesuada fames ac ante ipsum primis in faucibus. In purus mi, sagittis sed massa vel, consequat hendrerit erat. Mauris eu leo in mauris finibus ornare tempor vitae ligula. Phasellus faucibus at metus non sagittis. Donec vel fringilla lacus. Aliquam placerat orci at tempus gravida. Fusce sodales auctor erat, nec pharetra ante venenatis ut. Maecenas quis mauris non tellus blandit congue.</p>
+						<?=file_get_contents('./files/mitra/' . $mitra['file_artikel'])?>
 					</div>
 				</div>
 			</div> <!-- end col for article -->
